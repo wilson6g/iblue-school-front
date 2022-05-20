d7ee7f9ab0044b121a66f300ff858be1983e5510
wilson6g <wilsonpinheirodev@gmail.com>
2022-05-20 01:14:49 -0300

    Added tips

---
+++ src/components/TipComponent.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container-tip">
+    <strong class="title-tip"> Como usar as novas operações: </strong>
+    <div class="content-tip">
+      <p>
+        💡 Para utilizar porcentagem, siga o exemplo: <strong>100%15</strong>
+      </p>
+      <p>
+        💡 Para utilizar raiz quadrada, siga o exemplo: <strong>50√</strong>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TipComponent",
+};
+</script>
+
+<style scoped>
+strong {
+  color: var(--white);
+}
+
+@media (min-width: 100px) and (max-width: 767px) {
+  .container-tip {
+    width: 20vw;
+  }
+}
+
+@media (min-width: 768px) and (max-width: 1200px) {
+  .container-tip {
+    width: 35vw;
+  }
+}
+
+.title-tip {
+  text-align: center;
+  font-size: 24px;
+}
+
+.container-tip {
+  display: flex;
+  background: var(--radical-red);
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 30vw;
+  height: 50vh;
+  border-radius: 5px;
+  padding: 25px 0;
+}
+
+.content-tip {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 250px;
+  height: 250px;
+  color: var(--white);
+  text-align: justify;
+}
+</style>
--- src/views/AdvancedView.vue
+++ src/views/AdvancedView.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <HeaderComponent />
     <main class="main-content">
+      <TipComponent class="tip" />
       <AdvancedCalculatorComponent />
     </main>
     <FooterComponent />
@@ -12,6 +13,7 @@
 import HeaderComponent from "../components/HeaderComponent.vue";
 import FooterComponent from "../components/FooterComponent.vue";
 import AdvancedCalculatorComponent from "../components/AdvancedCalculatorComponent.vue";
+import TipComponent from "../components/TipComponent.vue";
 
 export default {
   name: "AdvancedView",
@@ -19,6 +21,7 @@ export default {
     HeaderComponent,
     FooterComponent,
     AdvancedCalculatorComponent,
+    TipComponent,
   },
 };
 </script>
@@ -28,8 +31,59 @@ export default {
   width: 100vw;
   height: 80vh;
   display: flex;
-  justify-content: center;
+  justify-content: space-evenly;
   align-items: center;
+}
+
+@media (min-width: 100px) and (max-width: 425px) {
+  .main-content {
+    padding: 15rem 0;
+    width: 85vw;
+    height: 80vh;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  .tip {
+    height: 35vh;
+    width: 79vw;
+    margin-bottom: 3rem;
+  }
+}
+
+@media (min-width: 426px) and (max-width: 767px) {
+  .main-content {
+    padding: 15rem 0;
+    width: 85vw;
+    height: 80vh;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  .tip {
+    height: 35vh;
+    width: 80vw;
+    margin-bottom: 3rem;
+  }
+}
+
+@media (min-width: 768px) and (max-width: 1200px) {
+  .main-content {
+    padding: 15rem 0;
+    width: 100vw;
+    height: 80vh;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+  }
+
+  .tip {
+    width: 35vw;
+  }
 }
 
 h1 {
